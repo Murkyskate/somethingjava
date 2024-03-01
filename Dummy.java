f3cc596e05a88639c5601c4b1d20cb1b1f534453
Kumar Pratyush <62933862+Murkyskate@users.noreply.github.com>
2024-03-01 19:19:26 +0530

    Create Dummy.java

---
+++ Dummy.java
@@ -0,0 +1,34 @@
+package com.accenture.demo;
+
+import javax.naming.AuthenticationException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public void signup(SignupRequest signupRequest) {
+        User user = new User();
+        user.setUsername(signupRequest.getUsername());
+        user.setPassword(passwordEncoder.encode(signupRequest.getPassword()));
+        userRepository.save(user);
+    }
+
+    public String login(LoginRequest loginRequest) throws AuthenticationException {
+        User user = userRepository.findByUsername(loginRequest.getUsername());
+        if (user == null || !passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())) {
+            throw new AuthenticationException("Invalid username or password");
+        }
+        return jwtTokenProvider.generateToken(user.getUsername());
+    }
+}
